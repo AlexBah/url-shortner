b14b717850f39be552823826e7508a21a43afca1
Alex Bah <P_osts@mail.ru>
2025-08-09 21:43:56 +0700

    #2

--- cmd/url-shortner/main.go
+++ cmd/url-shortner/main.go
@@ -4,49 +4,30 @@ import (
 	"log/slog"
 	"os"
 	"url-shortner/internal/config"
-)
-
-const (
-	envLocal = "local"
-	envDev   = "dev"
-	envProd  = "prod"
+	"url-shortner/internal/lib/logger/setuplogger"
+	"url-shortner/internal/lib/logger/sl"
+	"url-shortner/internal/storage/sqlite"
 )
 
 func main() {
 
 	cfg := config.MustLoad()
 
-	log := setupLogger(cfg.Env)
+	log := setuplogger.Setup(cfg.Env)
 
 	log.Info("starting url-shortner", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
 
-	//TODO: init logger: slog
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
 
-	//TODO: init storage: sqlight
+	_ = storage
 
 	//TODO: init router: chi
 
 	//TODO: run server:
 
-}
-
-func setupLogger(env string) *slog.Logger {
-	var log *slog.Logger
-
-	switch env {
-	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
-	case envDev:
-		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
-	case envProd:
-		log = slog.New(
-			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
-		)
-	}
-	return log
 }
