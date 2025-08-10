dff9dd8ae8844ac552cacce7c961b980cd494029
Alex Bah <P_osts@mail.ru>
2025-08-10 19:09:59 +0700

    #3

--- cmd/url-shortner/main.go
+++ cmd/url-shortner/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"log/slog"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"golang.org/x/exp/slog"
+
 	"url-shortner/internal/config"
+	"url-shortner/internal/http-server/handlers/url/save"
+	mwLogger "url-shortner/internal/http-server/middleware/logger"
 	"url-shortner/internal/lib/logger/setuplogger"
 	"url-shortner/internal/lib/logger/sl"
 	"url-shortner/internal/storage/sqlite"
@@ -24,10 +31,29 @@ func main() {
 		os.Exit(1)
 	}
 
-	_ = storage
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 
-	//TODO: init router: chi
+	router.Post("/url", save.New(log, storage))
 
-	//TODO: run server:
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
+	log.Error("server stopped")
 
 }
--- internal/lib/logger/setuplogger/setuplogger.go
+++ internal/lib/logger/setuplogger/setuplogger.go
@@ -1,8 +1,10 @@
 package setuplogger
 
 import (
-	"log/slog"
 	"os"
+	"url-shortner/internal/lib/logger/handlers/slogpretty"
+
+	"golang.org/x/exp/slog"
 )
 
 func Setup(env string) *slog.Logger {
@@ -15,9 +17,7 @@ func Setup(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -28,4 +28,16 @@ func Setup(env string) *slog.Logger {
 		)
 	}
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
